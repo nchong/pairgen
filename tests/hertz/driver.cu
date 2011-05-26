83a06b7de7a344e736b3ab4a2913cc0513507c72
Nathan Chong <nyc04@doc.ic.ac.uk>
2011-05-26 20:19:58 +0100

    Almost sorted out torque results. Need to check shear.

--- tests/hertz/driver.cu
+++ tests/hertz/driver.cu
@@ -9,14 +9,14 @@
 using namespace std;
 
 void run(struct params *input, int num_iter) {
-  one_time.push_back(SimpleTimer("hertz_constants"));
-  one_time.back().start();
   double dt = 0.00001;
   double nktv2p = 1;
   double yeff = 3134796.2382445144467056;
   double geff = 556173.5261401557363570;
   double betaeff = -0.3578571305033167;
   double coeffFrict = 0.5;
+  one_time.push_back(SimpleTimer("hertz_constants"));
+  one_time.back().start();
   hertz_setup_constants(dt, nktv2p, yeff, geff, betaeff, coeffFrict);
   one_time.back().stop_and_add_to_total();
 
@@ -61,27 +61,36 @@ void run(struct params *input, int num_iter) {
   hertz_init(input->nnode, NSLOT, input->radius, input->mass, input->type);
   one_time.back().stop_and_add_to_total();
 
-  hertz_run(input->nnode, NSLOT, numneigh, neighidx, input->x, input->v,
-      input->omega, input->force, input->torque, shear, touch);
+  per_iter.push_back(SimpleTimer("hertz_run"));
+  for (int run=0; run<num_iter; run++) {
+    per_iter[0].start();
+    hertz_run(input->nnode, NSLOT, numneigh, neighidx, input->x, input->v,
+        input->omega, input->force, input->torque, shear, touch);
+    per_iter[0].stop_and_add_to_total();
 
-  for (int n=0; n<input->nnode; n++) {
-    const double epsilon = 0.00001;
-    bool verbose = false;
-    bool die_on_flag = false;
+#if 0
+    if (run == 0) {
+      for (int n=0; n<input->nnode; n++) {
+        const double epsilon = 0.00001;
+        bool verbose = false;
+        bool die_on_flag = false;
 
-    stringstream out;
-    out << "force[" << n << "]";
-    check_result_vector(
-        out.str().c_str(),
-        &input->expected_force[(n*3)], &input->force[(n*3)], 
-        epsilon, verbose, die_on_flag);
-    out.str("");
+        stringstream out;
+        out << "force[" << n << "]";
+        check_result_vector(
+            out.str().c_str(),
+            &input->expected_force[(n*3)], &input->force[(n*3)], 
+            epsilon, verbose, die_on_flag);
+        out.str("");
 
-    out << "torque[" << n << "]";
-    check_result_vector(
-        out.str().c_str(),
-        &input->expected_torque[(n*3)], &input->torque[(n*3)],
-        epsilon, verbose, die_on_flag);
+        out << "torque[" << n << "]";
+        check_result_vector(
+            out.str().c_str(),
+            &input->expected_torque[(n*3)], &input->torque[(n*3)],
+            epsilon, verbose, die_on_flag);
+      }
+    }
+#endif
   }
 
   hertz_exit();
