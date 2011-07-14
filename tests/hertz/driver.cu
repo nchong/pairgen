e21131db30fa2798e5a238efe465944132da65c4
Nathan <nathan.chong@gmail.com>
2011-07-14 17:21:36 +0100

    Deal properly with neighbor list before and after kernel invocation.
    There are three(!) neighbor list representations.
    1) LAMMPS
    for ii in 0..inum:
      i = ilist[ii]               #< this mapping is identity for our inputs
      for jj in 0..numneigh[i]:
        j = firstneigh[i][jj]     #< where firstneigh[i] is a pointer into a set of pages
    2) UNPICKLE
    for e in 0..input->nedge:
      i = input->edge[(e*2)  ]
      j = input->edge[(e*2)+1]
    3) GPU
    for i in 0..N:
      for jj in 0..numneigh[i]:
        j = neighidx[(i*NSLOT)+jj]
    
    I don't know if it is necessary to load/unload per-neighbor data (neighidx, shear and touch) before/after each kernel invocation.
    It depends on if these values are required (and possibly mutated) between invocations.
    This is a problem that still needs to be solved.
    
    OTHER CHANGES
    1) Make copies of all data mutated by kernel invocation. This ensures that all kernel invocations receive the same input.
    2) Use input parameter file to initialize hertz constants.

--- tests/hertz/driver.cu
+++ tests/hertz/driver.cu
@@ -9,12 +9,15 @@
 using namespace std;
 
 void run(struct params *input, int num_iter) {
-  double dt = 0.00001;
-  double nktv2p = 1;
-  double yeff = 3134796.2382445144467056;
-  double geff = 556173.5261401557363570;
-  double betaeff = -0.3578571305033167;
-  double coeffFrict = 0.5;
+  // TODO: we only send scalar values as constants to the device.
+  // We take advantage of the fact that all particles are the same type
+  // So we only need one value from the yeff, geff, betaeff and coeffFrict arrays
+  double dt = input->dt;
+  double nktv2p = input->nktv2p;
+  double yeff = input->yeff[3];
+  double geff = input->geff[3];
+  double betaeff = input->betaeff[3];
+  double coeffFrict = input->coeffFrict[3];
   one_time.push_back(SimpleTimer("hertz_constants"));
   one_time.back().start();
   hertz_setup_constants(dt, nktv2p, yeff, geff, betaeff, coeffFrict);
@@ -71,38 +74,108 @@ void run(struct params *input, int num_iter) {
   hertz_update_neigh(input->nnode, NSLOT, numneigh, neighidx);
   one_time.back().stop_and_add_to_total();
 
+  //internal copies of data mutated by kernel
+  int *numneigh_copy = new int[input->nnode];
+  int *neighidx_copy = new int[input->nnode*NSLOT];
+  double *force_copy = new double[input->nnode*3];
+  double *torque_copy = new double[input->nnode*3];
+  double *shear_copy = new double[input->nnode*NSLOT*3];
+  int *touch_copy = new int[input->nnode*NSLOT];
+
   per_iter.push_back(SimpleTimer("hertz_run"));
+  per_iter.push_back(SimpleTimer("convert_neighbor"));
   for (int run=0; run<num_iter; run++) {
 
+    //make copies
+    copy(numneigh, numneigh + input->nnode, numneigh_copy);
+    copy(neighidx, neighidx + input->nnode*3, neighidx_copy);
+    copy(input->force, input->force + input->nnode*3, force_copy);
+    copy(input->torque, input->torque + input->nnode*3, torque_copy);
+    copy(shear, shear + input->nnode*NSLOT*3, shear_copy);
+    copy(touch, touch + input->nnode*NSLOT, touch_copy);
+
     per_iter[0].start();
-    hertz_run(input->nnode, NSLOT, numneigh, neighidx, input->x, input->v,
-        input->omega, input->force, input->torque, shear, touch);
+    hertz_run(input->nnode, NSLOT, numneigh_copy, neighidx_copy,
+        input->x, input->v, input->omega,
+        force_copy, torque_copy, shear_copy, touch_copy);
     per_iter[0].stop_and_add_to_total();
+
+    per_iter[1].start();
+    for (int ii=0; ii<nl->inum; ii++) {
+      int i = nl->ilist[ii];
+      for (int jj=0; jj<nl->numneigh[i]; jj++) {
+        int j = nl->firstneigh[i][jj];
+        if (i < j) {
+          int idx = (i*NSLOT) + jj;
+          nl->firstdouble[i][(jj*3)  ] = shear_copy[(idx*3)  ];
+          nl->firstdouble[i][(jj*3)+1] = shear_copy[(idx*3)+1];
+          nl->firstdouble[i][(jj*3)+2] = shear_copy[(idx*3)+2];
+          nl->firsttouch[i][jj] = touch_copy[idx];
+        }
+      }
+    }
+    per_iter[1].stop_and_add_to_total();
 
 #if 1
     if (run == 0) {
-      for (int n=0; n<input->nnode; n++) {
-        const double epsilon = 0.00001;
-        bool verbose = false;
-        bool die_on_flag = false;
+      const double epsilon = 0.00001;
+      bool verbose = false;
+      bool die_on_flag = false;
 
+      for (int n=0; n<input->nnode; n++) {
         stringstream out;
         out << "force[" << n << "]";
         check_result_vector(
             out.str().c_str(),
-            &input->expected_force[(n*3)], &input->force[(n*3)], 
+            &input->expected_force[(n*3)], &force_copy[(n*3)],
             epsilon, verbose, die_on_flag);
-        out.str("");
+      }
 
+      for (int n=0; n<input->nnode; n++) {
+        stringstream out;
         out << "torque[" << n << "]";
         check_result_vector(
             out.str().c_str(),
-            &input->expected_torque[(n*3)], &input->torque[(n*3)],
+            &input->expected_torque[(n*3)], &torque_copy[(n*3)],
             epsilon, verbose, die_on_flag);
       }
+
+      int ptr = 0;
+      double *shear_check = new double[input->nedge*3];
+      for (int ii=0; ii<nl->inum; ii++) {
+        int i = nl->ilist[ii];
+        for (int jj=0; jj<nl->numneigh[i]; jj++) {
+          int idx = (i*NSLOT)+jj;
+          shear_check[(ptr*3)  ] = shear_copy[(idx*3)  ];
+          shear_check[(ptr*3)+1] = shear_copy[(idx*3)+1];
+          shear_check[(ptr*3)+2] = shear_copy[(idx*3)+2];
+          ptr++;
+        }
+      }
+      verbose = true;
+      for (int n=0; n<input->nedge; n++) {
+        stringstream out;
+        out << "shear[" << n << "]";
+        check_result_vector(
+            out.str().c_str(),
+            &input->expected_shear[(n*3)], &shear_check[(n*3)],
+            epsilon, verbose, die_on_flag);
+      }
+      delete[] shear_check;
     }
 #endif
   }
 
   hertz_exit();
+  delete[] numneigh;
+  delete[] neighidx;
+  delete[] shear;
+  delete[] touch;
+
+  delete[] numneigh_copy;
+  delete[] neighidx_copy;
+  delete[] force_copy;
+  delete[] torque_copy;
+  delete[] shear_copy;
+  delete[] touch_copy;
 }
