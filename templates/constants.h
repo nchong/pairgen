87088932321715d50d055e79a0a38d6ad9456661
Nathan <nathan.chong@gmail.com>
2011-07-14 22:00:34 +0100

    Split constants into .h and .cu

--- templates/constants.h
+++ templates/constants.h
@@ -9,10 +9,6 @@ void {{name}}_setup_constants(
   {% for c in consts -%}
     {{ c.type }} {{ c.emit_name(name_prefix='h_') }}{{ ',' if not loop.last }}
   {% endfor %}
-  ) {
-  {% for c in consts -%}
-    cudaMemcpyToSymbol("{{ c.device_name() }}", &{{ c.emit_name(name_prefix='h_') }}, {{ c.sizeof() }}, 0, cudaMemcpyHostToDevice);
-  {% endfor %}
-}
+  );
 
 #endif
